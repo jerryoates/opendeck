9b312ac05bcb082acbf76c937af2b5602cd56d5d
jerryoates <jerryoates1@gmail.com>
2024-02-29 23:48:36 -0500

    remove form tag to fix rerender

--- apps/dims/src/app/page.tsx
+++ apps/dims/src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client"
-import Image from "next/image";
-import { useState, FormEvent } from "react";
+import { useState } from "react";
 
 export default function Home() {
 
     const [addFreightMenuOpen, setAddFreightMenuOpen] = useState(false)
+
+    const [currentFreight, setCurrentFreight]: any[] = useState([])
 
     async function onFreightSubmit(event: string) {
         // event.preventDefault()
@@ -13,14 +14,14 @@ export default function Home() {
 
         // console.log('Form Data: ', formData)
 
-        const response = await fetch('/api', {
-            method: 'POST',
-            body: event,
-        })
+        // const response = await fetch('/api', {
+        //     method: 'POST',
+        //     body: event,
+        // })
 
-        // Handle response if necessary
-        const data = await response.json()
-        // ...
+        // // Handle response if necessary
+        // const data = await response.json()
+        // // ...
     }
 
     const formStyle = {
@@ -28,8 +29,8 @@ export default function Home() {
     }
 
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <main className="min-h-screen flex-col items-center justify-between p-24">
+            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
                 <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
                     Welcome to Open Deck's Dimension Calculator
                 </p>
@@ -41,28 +42,47 @@ export default function Home() {
                     {!addFreightMenuOpen ? "Add Freight +" : "Close Menu X"}
                 </button>
             </div>
-            {addFreightMenuOpen && (
-                <form className="flex-col text-lg" onSubmit={() => onFreightSubmit("test form:")}>
-                    {"Name:   "}
-                    <input type="text" name="name" style={formStyle} />
-                    <br />
-                    {"Quantity:   "}
-                    <input type="number" name="name" style={formStyle} />
-                    <br />
-                    {"Length (in):   "}
-                    <input type="number" name="length" style={formStyle} />
-                    <br />
-                    {"Width (in):   "}
-                    <input type="number" name="height" style={formStyle} />
-                    <br />
-                    {"Height (in):   "}
-                    <input type="quantity" name="width" style={formStyle} />
-                    <br />
-                    <button type="submit" className="justify-center border-b border-gray-300 bg-gradient-to-b"> Save Load </button>
-                </form>
-            )}
             <div>
+                {
+                    currentFreight.length ? currentFreight.map((freight: any) => {
+                        return (
+                            <div> {freight} </div>
+                        )
+                    }) : null
+                }
             </div>
+            {addFreightMenuOpen && (
+                <div>
+                    <div className="text-lg">
+                        {"Name:   "}
+                        <input type="text" name="name" style={formStyle} />
+                        <br />
+                        {"Quantity:   "}
+                        <input type="number" name="name" style={formStyle} />
+                        <br />
+                        {"Length (in):   "}
+                        <input type="number" name="length" style={formStyle} />
+                        <br />
+                        {"Width (in):   "}
+                        <input type="number" name="height" style={formStyle} />
+                        <br />
+                        {"Height (in):   "}
+                        <input type="quantity" name="width" style={formStyle} />
+                        <br />
+                        <button onClick={() =>
+                            setCurrentFreight([...currentFreight, 'new item'])
+                        }
+                            className="justify-center border-b border-gray-300 bg-gradient-to-b"> Save Freight
+                        </button>
+                        <br />
+                        {
+                            currentFreight.length ? (
+                                <button type="submit" onClick={() => onFreightSubmit("test form:")} className="justify-center border-b border-gray-300 bg-gradient-to-b"> Calculate </button>
+                            ) : null
+                        }
+                    </div>
+                </div>
+            )}
         </main>
     );
 }
