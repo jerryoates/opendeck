10e27223718cd0a0a246efd3101c018f55d7f096
jerryoates <jerryoates1@gmail.com>
2024-03-01 01:56:27 -0500

    add trailer as type

--- apps/dims/src/app/api/route.ts
+++ apps/dims/src/app/api/route.ts
@@ -1,18 +1,25 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { NextResponse } from "next/server";
+import Trailer from '@dims/types/trailer';
+
+type Flatbed48 = Trailer
+
+const flatbed48 = {
+    length: 576,
+    width: 102,
+    height: 102,
+    carryingCapacity: 48000,
+}
  
 export async function POST(
 //   req: NextRequest,
     req: any,
-  res: NextApiResponse
+    res: NextApiResponse
 ) {
     const data = await req.json()
     console.log(data)
-//   const data = await req.body.json()
 
-//   console.log('DATA: ', JSON.parse(data))
+    // loop through freight... calculate volume and weight separately
 
-//   console.log("data: ", JSON.parse(data))
-//   const id = await createItem(data)
   return NextResponse.json({ message: data }, { status: 200 });
 }
---
+++ apps/dims/src/types/trailer.d.ts
@@ -0,0 +1,6 @@
+export default interface Trailer {
+    length: number,
+    width: number,
+    height: number
+    carryingCapacity: number,
+}
