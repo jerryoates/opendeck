d3ceb844bd76aea7c5768ddecc49ef1f4a58096d
jerryoates <jerryoates1@gmail.com>
2024-03-08 20:18:41 -0500

    grid setup

--- apps/dims/src/app/page.tsx
+++ apps/dims/src/app/page.tsx
@@ -67,22 +67,98 @@ export default function Home() {
     }
 
     const deleteButtonStyle = {
-        marginLeft: 200
+        marginLeft: 20
     }
 
     return (
         <main className="min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            <div className="z-10 max-w-5xl w-full tems-center justify-between font-mono text-sm">
+                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 text-lg">
                     Welcome to Open Deck&apos;s Dimension Calculator
                 </p>
+                <div className="grid grid-cols-6 gap-4 my-8 font-bold text-md">
+                    <div>Name</div>
+                    <div>Quantity</div>
+                    <div>Length (in)</div>
+                    <div>Width (in)</div>
+                    <div>Height (in)</div>
+                    <div>Weight (lbs)</div>
+                </div>
+                <div className="grid grid-cols-6 gap-4 mb-5">
+                    {
+                        savedFreight.length ? savedFreight.map((freight: ActiveFreight, index: number) => {
+                            return (
+                                <>
+                                    <div>
+                                        {`${freight.name}`}
+                                    </div>
+                                    <div>
+                                        {`${freight.quantity}`}
+                                    </div>
+                                    <div>
+                                        {`${freight.length} in`}
+                                    </div>
+                                    <div>
+                                        {`${freight.width} in`}
+                                    </div>
+                                    <div>
+                                        {`${freight.height} in`}
+                                    </div>
+                                    <div>
+                                        {`${freight.weight} lbs`}
+                                        <button
+                                            style={deleteButtonStyle}
+                                            onClick={() => {
+                                                const previousFreight = [...savedFreight]
+                                                const newFreight = previousFreight.filter(item => item.name !== freight.name)
+                                                setSavedFreight(newFreight)
+                                            }}
+                                        > X </button>
+                                    </div>
+                                </>
 
-                <button
-                    className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-                    onClick={() => setAddFreightMenuOpen(!addFreightMenuOpen)}
-                >
-                    {!addFreightMenuOpen ? "Add Freight +" : "Close Menu X"}
-                </button>
+                            )
+                        }) : null
+                    }
+                    {!addFreightMenuOpen ? (
+                        <button
+                            className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+                            onClick={() => setAddFreightMenuOpen(!addFreightMenuOpen)}
+                        >
+                            Add Freight +
+                        </button>
+                    ) : <>
+                        <input type="text" name="name" style={formStyle}
+                            value={activeFreight.name}
+                            onChange={event => setActiveFreight({ ...activeFreight, name: event.target.value })}
+                        />
+                        <input type="number" name="quantity" style={formStyle}
+                            value={activeFreight.quantity || ''}
+                            onChange={event => setActiveFreight({ ...activeFreight, quantity: Number(event.target.value) })}
+                        />
+                        <input type="number" name="length" style={formStyle}
+                            value={activeFreight.length || ''}
+                            onChange={event => setActiveFreight({ ...activeFreight, length: Number(event.target.value) })}
+                        />
+                        <input type="number" name="width" style={formStyle}
+                            value={activeFreight.height || ''}
+                            onChange={event => setActiveFreight({ ...activeFreight, height: Number(event.target.value) })}
+                        />
+                        <input type="number" name="height" style={formStyle}
+                            value={activeFreight.width || ''}
+                            onChange={event => setActiveFreight({ ...activeFreight, width: Number(event.target.value) })}
+                        />
+                        <input type="number" name="weight" style={formStyle}
+                            value={activeFreight.weight || ''}
+                            onChange={event => setActiveFreight({ ...activeFreight, weight: Number(event.target.value) })}
+                        />
+                    </>
+
+                    }
+                </div>
+                <div className="grid grid-cols-5 gap-4 my-5">
+
+                </div>
                 {
                     responseAttempted && calculatedResponse && (
                         <div className="justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -118,7 +194,7 @@ export default function Home() {
                 }
             </div>
             <div>
-                {
+                {/* {
                     savedFreight.length ? savedFreight.map((freight: ActiveFreight, index: number) => {
                         return (
                             <li key={index} style={formStyle}>
@@ -142,7 +218,7 @@ export default function Home() {
                             </li>
                         )
                     }) : null
-                }
+                } */}
             </div>
             {
                 addFreightMenuOpen && (
