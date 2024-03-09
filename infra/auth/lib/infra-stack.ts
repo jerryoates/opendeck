2e88a1b27599baef2b2a32515fbf38f0fda81c13
jerryoates <jerryoates1@gmail.com>
2024-03-09 05:13:08 -0500

    use node mods

--- infra/auth/lib/infra-stack.ts
+++ infra/auth/lib/infra-stack.ts
@@ -1,8 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cognito from "aws-cdk/cognito"
 
 export class InfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
