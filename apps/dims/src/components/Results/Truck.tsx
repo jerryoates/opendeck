9c24969d65c84e7e61c68f5646b1f6fe1d6cb139
jerryoates <jerryoates1@gmail.com>
2024-05-17 21:54:55 -0400

    response formatting

--- apps/dims/src/components/Results/Truck.tsx
+++ apps/dims/src/components/Results/Truck.tsx
@@ -6,18 +6,14 @@ export default function Truck({ truck, index }: any) {
             <div className="font-bold">
                 {`${index + 1}. ${truck.trailer.name} - ${truck.items.length} items -`}
                 &nbsp;&nbsp;&nbsp;&nbsp;
-                {`${numberWithCommas(truck.currentCapacity)}lbs / ${numberWithCommas(truck.trailer.carryingCapacity)}lbs`}
+                {`${numberWithCommas(truck.currentCapacity)} / ${numberWithCommas(truck.trailer.carryingCapacity)}lbs`}
                 &nbsp;&nbsp;&nbsp;&nbsp;
-                {`${truck.areaUsed} / ${truck.trailer.area}(sq. in)`}
-                {/* &nbsp;&nbsp;&nbsp;&nbsp;
-                {`Length - ${truck.lengthUsed} / ${truck.trailer.length}(in)`}
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                {`Width - ${truck.widthUsed} / ${truck.trailer.width}(in)`} */}
+                {`${numberWithCommas(truck.areaUsed)} / ${numberWithCommas(truck.trailer.area)}(sq. in)`}
             </div>
             {
                 truck.items.map(({ item, position }: { item: any, position: any }, index: number) =>
                     <div key={index}>
-                        {`${item.name} - ${item.length}in/${item.width}in/${item.weight}lbs`}
+                        {`${item.name} - ${item.length}in/${item.width}in/${numberWithCommas(item.weight)}lbs`}
                     </div>
                 )
             }
