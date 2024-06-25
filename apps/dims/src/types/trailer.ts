427828a2df8395820f6be78e59b28d68ec687c37
jerryoates <jerryoates1@gmail.com>
2024-06-24 21:20:52 -0400

    add type for flatbed 53

--- apps/dims/src/types/trailer.ts
+++ apps/dims/src/types/trailer.ts
@@ -1,21 +1,29 @@
-interface Trailer {
-    name: string
-    length: number,
-    width: number,
-    height: number,
-    area: number
-    carryingCapacity: number,
+export interface Trailer {
+  name: string;
+  length: number;
+  width: number;
+  height: number;
+  area: number;
+  carryingCapacity: number;
+  cost: number;
 }
 
-type Flatbed48 = Trailer
+export const flatbed48: Trailer = {
+  name: 'Flatbed 48',
+  length: 576, // 48 feet in inches
+  width: 102,  // 8.5 feet in inches
+  height: 102,
+  area: 576 * 102,
+  carryingCapacity: 48000, // 48,000 lbs
+  cost: 100 // Example cost
+};
 
-const flatbed48: Flatbed48 = {
-    name: 'Flatbed 48',
-    length: 576,
-    width: 102,
-    area: 58752,
-    height: 102,
-    carryingCapacity: 48000,
-}
-
-export { type Trailer, flatbed48 }
+export const flatbed53: Trailer = {
+  name: 'Flatbed 53',
+  length: 636, // 53 feet in inches
+  width: 102,
+  height: 102,
+  area: 636 * 102,
+  carryingCapacity: 53000, // 53,000 lbs
+  cost: 120 // Example cost
+};
